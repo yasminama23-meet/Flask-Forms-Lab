7b0f5e6e1c9057373ee4bba81e7758f58fd97e92
yasminama23-meet <109900009+yasminama23-meet@users.noreply.github.com>
2022-07-28 17:40:13 +0300

    /.

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -2,35 +2,43 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
 app = Flask(  # Create a flask app
-	_name_,
+	__name__,
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
 
 username = "yasmina"
 password = "12345"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["leen","bean","finly", "frankly", "giries", "jamal"]
 
 
 @app.route('/')  # '/' for the default page
 def login():
 	if request.method == 'POST':
+
 		if username == request.form['username'] and password == request.form['password']:
+
 			return redirect(url_for('Home'))
 		else:
+
 			return render_template('login.html')
+
 	return render_template('login.html')
+
+
+
+
 
 @app.route('/home')
 def Home():
-	return render_template('home.html')
+	return render_template('home.html',facebook_friends=facebook_friends)
 
 
 
 
 
 
-if _name_ == "_main_":  # Makes sure this is the main process
+if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
     debug=True
 	)
