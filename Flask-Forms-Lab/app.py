a9c244ed52dfa91e0e656d21fe6b5b020d58d3cb
yasminama23-meet <109900009+yasminama23-meet@users.noreply.github.com>
2022-07-28 17:20:49 +0300

    .

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -2,32 +2,35 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
 app = Flask(  # Create a flask app
-	__name__,
+	_name_,
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
 
-
-username = "llo2ay"
-password = "123"
+username = "yasmina"
+password = "12345"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/',methods=['GET', 'POST'])  # '/' for the default page
+@app.route('/')  # '/' for the default page
 def login():
-	if request.method == 'GET':
-		return render_template('login.html')
-	else:
-		
-  return render_template('home.html')
+	if request.method == 'POST':
+		if username == request.form['username'] and password == request.form['password']:
+			return redirect(url_for('Home'))
+		else:
+			return render_template('login.html')
+	return render_template('login.html')
+
+@app.route('/home')
+def Home():
+	return render_template('home.html')
 
 
 
-  
 
 
 
-if __name__ == "__main__":  # Makes sure this is the main process
+if _name_ == "_main_":  # Makes sure this is the main process
 	app.run( # Starts the site
     debug=True
 	)
